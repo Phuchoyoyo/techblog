d8e60b180409cdf7cb065d39611cca8d939da6f6
Phuchoyoyo <91901988+Phuchoyoyo@users.noreply.github.com>
2021-11-19 11:15:23 -0500

    Update html.js

--- html.js
+++ html.js
@@ -2,17 +2,20 @@
 
 <html>
   <head>
-  <title> hell0 </title>
+  <title> javascript notes</title>
   </head>
-<body>
-</body>
-</html>
-<!DOCTYPE html>
 
-<html>
-  <head>
-  <title></title>
-  </head>
 <body>
+    
+    <p class="codepen" data-height="300" data-theme-id="dark" data-default-tab="html,result" data-slug-hash="OJjdEbV" data-user="phuchoyoyo" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/phuchoyoyo/pen/OJjdEbV">
+  js-operator</a> by Phuchoyoyo (<a href="https://codepen.io/phuchoyoyo">@phuchoyoyo</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+
+
 </body>
 </html>
+
+
